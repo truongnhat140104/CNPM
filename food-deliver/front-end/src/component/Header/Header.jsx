28a04dfdba0712cee6052a9b89d2d338d5e3f4f0
truongnhat140104 <elvis140104@gmail.com>
2025-10-03 00:19:08 +0700

    them folder Header, file .jsx, .css

---
+++ food-deliver/front-end/src/component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order ur favorfood here</h2>
+            <p>alskdjádjalsdalsdjlkj</p>
+            <button>View menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
