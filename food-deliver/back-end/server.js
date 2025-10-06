97ae33882666331deda5aa2b7d24abb6701e096e
Diệu Linh <dieulinh130504@gmail.com>
2025-10-06 16:37:03 +0700

    setup server BE

--- food-deliver/back-end/server.js
+++ food-deliver/back-end/server.js
@@ -1,3 +1,18 @@
 import exxpress from 'express';
 import cors from 'cors';
 
+//app config
+const app = exxpress();
+const port = 3000;
+
+//middlewares
+app.use(exxpress.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("API Working");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
