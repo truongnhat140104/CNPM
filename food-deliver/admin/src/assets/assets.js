e019a813ff843adcaf3fd82e80e00ee0b5c49483
truongnhat140104 <elvis140104@gmail.com>
2025-10-16 00:05:05 +0700

    fix port API

--- food-deliver/admin/src/assets/assets.js
+++ food-deliver/admin/src/assets/assets.js
@@ -5,6 +5,8 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+
+export const currency = ' vnđ'
 export const assets ={
     logo,
     add_icon,
