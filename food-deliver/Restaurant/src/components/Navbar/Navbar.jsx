5e865026204dbee3efeb0dc24665c90ad371e7e9
dieulinh1305 <dieulinh130504@gmail.com>
2025-11-12 00:48:02 +0700

    set up for Restaurants

--- food-deliver/Restaurant/src/components/Navbar/Navbar.jsx
+++ food-deliver/Restaurant/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img className="logo" src={assets.logo1} alt="" />
-      <p>Admin Panel</p>
+      <p>Restaurant Controller</p>
       {/* <img className='profile' src={assets.profile_image} alt="" /> */}
     </div>
   )
