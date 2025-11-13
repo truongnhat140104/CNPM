37176f38398907c4127d37c892ecc14d593d2807
truongnhat140104 <elvis140104@gmail.com>
2025-11-13 09:39:19 +0700

    add page and account for Res

--- food-deliver/Restaurant/src/App.jsx
+++ food-deliver/Restaurant/src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import Update from './pages/Update/Update'
+import Account from './pages/Account/Account'
 
 const App = () => {
 
@@ -9,13 +16,17 @@ const App = () => {
 
   return (
     <div>
-      {/* <ToastContainer/> */}
+      <ToastContainer/>
       <Navbar />
       <hr />
       <div className='app-content'>
         <Sidebar />
         <Routes>
-          {/* Define your restaurant routes here */}
+          <Route path="/add" element={<Add url={url} />} />
+          <Route path="/list" element={<List url={url} />} />
+          <Route path="/orders" element={<Orders url={url} />} />
+          <Route path="/update" element={<Update url={url} />} />
+          <Route path="/account" element={<Account url={url} />} /> 
         </Routes>
       </div>
     </div>
